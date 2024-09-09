22e5a6d7d4677381508ad088b44fc71313d514a0
aidaraitymkulov <aitymkulovich@gmail.com>
2024-09-09 16:52:31 +0600

    styled Homepage and created counter

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,14 +3,18 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store/store';
 import { TaskItem } from '../../components/TaskItem';
 import { removeTask, toggleTask } from '../../store/taskSlise';
+import styles from './Home.module.scss'
 
 const Home: React.FC = () => {
   const tasks = useSelector((state: RootState) => state.tasks.tasks);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Task List</h1>
+    <div className={styles.listBox}>
+      <div className={styles.head}>
+      <h1 className={styles.title}>Tasks</h1>
+      <div className={styles.counter}><span className={styles.number}>{tasks.length}</span></div>
+      </div>
       {tasks.map(task => (
         <TaskItem
           key={task.id}
